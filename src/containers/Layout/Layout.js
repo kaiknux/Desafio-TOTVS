f7076c8391e8229ce4cdd23251b6526d325e8e5b
eron c <deandradeeron@gmail.com>
2019-09-05 14:23:07 -0300

    Initial component structure
    Folders for future components to be built

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import classes from './Layout.module.css';
+
+class Layout extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div className={classes.layContainer}>
+            Layout
+            </div>
+        )
+    }
+}
+
+export default Layout;
